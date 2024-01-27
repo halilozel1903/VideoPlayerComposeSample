cf8df40abd5277fcc066589960b08edfb34234a6
Halil Ozel <halilozel1903@gmail.com>
2024-01-27 17:25:53 +0300

    Shape added.

---
+++ app/src/main/java/com/halil/ozel/videoplayercomposesample/ui/theme/Shape.kt
@@ -0,0 +1,15 @@
+package com.halil.ozel.videoplayercomposesample.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+/**
+ * Created by halilozel1903 on 27.01.2024.
+ */
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
