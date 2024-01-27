5e8e320d7856bc0d414d1c5a6407cc9a624a2708
Halil Ozel <halilozel1903@gmail.com>
2024-01-27 16:50:57 +0300

    App created.

---
+++ app/src/main/java/com/halil/ozel/videoplayercomposesample/VideoPlayerApp.kt
@@ -0,0 +1,10 @@
+package com.halil.ozel.videoplayercomposesample
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by halilozel1903 on 27.01.2024.
+ */
+@HiltAndroidApp
+class VideoPlayerApp: Application()
